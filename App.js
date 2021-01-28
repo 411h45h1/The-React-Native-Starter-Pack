87e95878b9e9ecd30ea6b8d97824254bdd5b8651
Ahmed <ahmedali.96@hotmail.com>
2021-01-28 01:34:32 -0500

    created base

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import AppState from "./src/core/context/AppState";
+import Screens from "./src/core/Screens";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => (
+  <AppState>
+    <Screens />
+  </AppState>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/core/context/AppState.js
@@ -0,0 +1,49 @@
+import React, { useReducer, useCallback, useEffect } from "react";
+import { StatusBar } from "react-native";
+
+//context
+import AppContext from "./appContext";
+import appReducer from "./appReducer";
+
+//theme
+import ThemeContext, { theme } from "../ThemeContext";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const AppState = (props) => {
+  const initialState = {
+    lightTheme: false,
+  };
+  const [state, dispatch] = useReducer(appReducer, initialState);
+  const { lightTheme } = state;
+
+  //Theme
+  const themeSwitch = async () => {
+    await AsyncStorage.setItem("theme", JSON.stringify(!lightTheme)).then(() =>
+      dispatch({ type: "CHANGE_THEME" })
+    );
+  };
+
+  const setStoredTheme = async () => {
+    await AsyncStorage.setItem("theme", JSON.stringify(false));
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        lightTheme: state.lightTheme,
+        themeSwitch,
+        setStoredTheme,
+      }}
+    >
+      <ThemeContext.Provider
+        value={state.lightTheme ? theme.light : theme.dark}
+      >
+        <StatusBar
+          barStyle={state.lightTheme ? "dark-content" : "light-content"}
+        />
+        {props.children}
+      </ThemeContext.Provider>
+    </AppContext.Provider>
+  );
+};
+export default AppState;
